0a41608cb14c7460867923e48ebdc969b71ac5f3
Chris Thomas <webmaster102@gmail.com>
2018-10-30 14:29:34 -0400

    added directives and event listeners

--- main.js
+++ main.js
@@ -1,29 +1,18 @@
 const app = new Vue({
   el: "#app",
   data: {
-    // name: "Chris",
-    // age: 28
-    chris: {
-      first: "Chris",
-      last: "Thomas",
-      age: 28
-    },
-    shawn: {
-      first: "Shawn",
-      last: "Abraham",
-      age: 29
-    }
-  },
-  computed: {
-    // chrisFullName() {
-    //   return `${this.chris.first} ${this.chris.last}`
-    // },
-    // shawnFullName() {
-    //   return `${this.shawn.first} ${this.shawn.last}`
-    // },
-    chrisAgeInOneYear() {
-      return this.chris.age + 1;
-    }
+    friends: [
+      {
+        first: "Chris",
+        last: "Thomas",
+        age: 28
+      },
+      {
+        first: "Shawn",
+        last: "Abraham",
+        age: 29
+      }
+    ]
   },
   filters: {
     ageInOneYear(age) {
@@ -33,12 +22,24 @@ const app = new Vue({
       return `${value.first} ${value.last}`;
     }
   },
+  methods: {
+    decrementAge(friend) {
+      friend.age = friend.age - 1;
+    },
+    incrementAge(friend) {
+      friend.age = friend.age + 1;
+    }
+  },
   template: `
     <div>
-      <h2>Name: {{chris | fullName}}</h2>
-      <h2>Age: {{chris.age | ageInOneYear}}</h2>
-      <h2>Name: {{shawn | fullName}}</h2>
-      <h2>Age: {{shawn.age | ageInOneYear}}</h2>
+      <h2 v-for="friend in friends">
+        <h4>{{friend | fullName}}</h4>
+        <h5>Age: {{friend.age}}</h5>
+        <button v-on:click="decrementAge(friend)">-</button>
+        <button v-on:click="incrementAge(friend)">+</button>
+        <input v-model="friend.first">
+        <input v-model="friend.last">
+      </h2>
     </div>
   `
 })
