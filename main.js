39521e6a8a40be77acebac25d7904e9f0dbd6720
Chris Thomas <webmaster102@gmail.com>
2018-10-30 14:04:35 -0400

    finished learncodeacademy's first lesson

--- main.js
+++ main.js
@@ -1,17 +1,23 @@
 const app = new Vue({
+  el: "#app",
   data: {
-    friends: []
-  },
-  mounted() {
-    fetch('http://rest.learncode.academy/api/vue-/friends')
-      .then(response => response.json())
-      .then(data => {
-        this.friends = data;
-      });
+    // name: "Chris",
+    // age: 28
+    chris: {
+      name: "Chris Thomas",
+      age: 28
+    },
+    shawn: {
+      name: "Shawn Abraham",
+      age: 29
+    }
   },
   template: `
     <div>
-      <li v-for="friend in friends">{{friend.name}}</li>
+      <h2>Name: {{chris.name}}</h2>
+      <h2>Age: {{chris.age}}</h2>
+      <h2>Name: {{shawn.name}}</h2>
+      <h2>Age: {{shawn.age}}</h2>
     </div>
   `
-});
+})
