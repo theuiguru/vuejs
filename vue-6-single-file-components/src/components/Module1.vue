5ab8c0b7db405973a1b9c536d30d8074d693fb8e
Chris Thomas <webmaster102@gmail.com>
2018-10-30 16:20:34 -0400

    scaled vue with single file components

---
+++ vue-6-single-file-components/src/components/Module1.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>Module 1</h1>
+</template>
+<script>
+export default {};
+</script>
+<style scoped>
+h1 {
+  color: red;
+}
+</style>
---
+++ vue-6-single-file-components/src/components/Module2.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>Module 2</h1>
+</template>
+<script>
+export default {};
+</script>
+<style scoped>
+h1 {
+  color: pink;
+}
+</style>
